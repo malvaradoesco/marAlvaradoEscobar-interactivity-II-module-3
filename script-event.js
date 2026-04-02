0ddfd774a5201abd3a507feafc38e0b8606c34d5
malvaradoesco <dalvaradoesco@gmail.com>
2026-04-02 10:17:30 -0400

    kaelyn

---
+++ script-event.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('thisButton');
+
+console.log("button",button);
+
+button.addEventListener("click", clickThisButton);
+
+function clickThisButton(){
+    alert("hello gays");
+}
