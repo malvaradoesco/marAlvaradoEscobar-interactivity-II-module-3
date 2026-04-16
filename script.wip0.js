fe06713b0b12819e21e193f179825509a291e843
malvaradoesco <dalvaradoesco@gmail.com>
2026-04-16 18:01:49 -0400

    styling and editing the code
    Co-authored-by: Leilei <lilyxia99@users.noreply.github.com>

--- script.wip0.js
+++ script.wip0.js
@@ -21,18 +21,34 @@ function openNextButton(nextButtonId){
     brightness -= 50;
 }
 
+function openNextButton2(nextButtonId){
+    let nextbuttonElement =document.getElementById(nextButtonId);nextbuttonElement.classList.remove('hidden');
+
+    let color = `rgba(${brightness},${brightness},${brightness})`;
+
+    nextButtonElement.style.backgroundColor = color;
+
+    brightness -= 50;
+}
+
 function ummAlert(){
     alert("You don't have a say in this.");
 }
 
 function popitup(url,windowName) {
-       newwindow=window.open(url,windowName,'height=200,width=150');
+       newwindow=window.open(url,windowName,'height=200,width=150,screenX=1000,screenY=200');
+       if (window.focus) {newwindow.focus()}
+       return false;
+     }
+
+function popitup1(url,windowName) {
+       newwindow=window.open(url,windowName,'height=200,width=150,screenX=325,screenY=600');
        if (window.focus) {newwindow.focus()}
        return false;
      }
 
 function popitLarge(url,windowName) {
-       newwindow=window.open(url,windowName,'height=400,width=300');
+       newwindow=window.open(url,windowName,'height=400,width=500, screenX=325,screenY=200');
        if (window.focus) {newwindow.focus()}
        return false;
      }
